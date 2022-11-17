b78e804f5a8b194a3f34574e08b02fac49a5074f
Anshuman Raina <anshofficial6898@gmail.com>
2022-11-17 21:25:55 +0530

    Time: 105 ms (57.58%), Space: 14 MB (71.27%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.py
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -2,10 +2,18 @@ class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
         A = (ax2-ax1)*(ay2-ay1)
         B = (bx2-bx1)*(by2-by1)
-        width_overlap = min(ax2,bx2) - max(ax1,bx1)
-        height_overlap = min(ay2,by2) - max(ay1,by1)
+        #width of overlap
+        left = max(ax1,bx1)
+        right = min(ax2,bx2)
+        width_overlap = right - left
+        #height of overlap
+        top = min(ay2,by2)
+        bottom = max(ay1,by1)
+        height_overlap = top - bottom
+        #area of overlap
         overlap = 0
         if width_overlap > 0 and height_overlap > 0:
             overlap = width_overlap*height_overlap
+        #total area
         total_area = A + B - overlap
         return total_area
